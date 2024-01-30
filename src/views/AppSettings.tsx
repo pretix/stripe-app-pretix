aa538f569d4c62e42f03ff44c3ffcc3c2cbff35d
Raphael Michel <michel@rami.io>
2024-01-30 13:24:33 +0100

    Add how to disconnect

--- src/views/AppSettings.tsx
+++ src/views/AppSettings.tsx
@@ -34,6 +34,33 @@ const AppSettings = () => {
 					pretix event at Setings → Payment → Stripe.
                 </Box>
             </Box>
+            <Box
+                css={{
+                    background: "container",
+                    padding: "large",
+                    borderRadius: "medium",
+                    width: "fit"
+                }}
+            >
+                <Box
+                    css={{
+                        font: "heading",
+                        stack: "x",
+                        alignY: "center",
+                        gap: "small",
+                        marginBottom: "small",
+                    }}
+                >
+                    <Icon name="trash" />
+                    How to disconnect
+                </Box>
+                <Box>
+					You can disconnect the same way as you connect, just remove the API keys from
+					your pretix event at Settings → Payment → Stripe and pretix will make no more
+					requests to Stripe. If you want to fully remove access, use the "Uninstall"
+					button above.
+                </Box>
+            </Box>
         </SettingsView>
     );
 };
