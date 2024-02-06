99579b3540823a2f15577507581014ea571274a7
Raphael Michel <michel@rami.io>
2024-02-06 17:46:52 +0100

    Ask users to create a webhook

--- src/views/AppSettings.tsx
+++ src/views/AppSettings.tsx
@@ -1,22 +1,18 @@
 import {
     Box,
-    Button,
+    Link,
     Divider,
     Icon,
+    Inline,
     SettingsView
 } from "@stripe/ui-extension-sdk/ui";
 
 const AppSettings = () => {
     return (
         <SettingsView>
-            <Box
-                css={{
-                    background: "container",
-                    padding: "large",
-                    borderRadius: "medium",
-                    width: "fit"
-                }}
-            >
+            <Box css={{
+                marginBottom: "large",
+            }}>
                 <Box
                     css={{
                         font: "heading",
@@ -26,22 +22,39 @@ const AppSettings = () => {
                         marginBottom: "small",
                     }}
                 >
-                    <Icon name="sparkle" />
+                    <Icon name="sparkle"/>
                     Welcome!
                 </Box>
-                <Box>
-					There are no settings for pretix here. You can configure the integration in your
-					pretix event at Setings → Payment → Stripe.
+                <Box css={{
+                    marginBottom: "small",
+                }}>
+                    <Inline css={{fontWeight: "bold"}}>Step 1: </Inline>
+                    Configure the integration in your pretix event at "Settings" → "Payment" → "Stripe".
+                </Box>
+                <Box css={{
+                    marginBottom: "small",
+                }}>
+                    <Inline css={{fontWeight: "bold"}}>Step 2: </Inline>
+                    Go to your <Link href="https://dashboard.stripe.com/webhooks">Webhook settings</Link> in Stripe
+                    dashboard and create a webhook that points to:
+                </Box>
+                <Box css={{
+                    marginBottom: "small",
+                    background: "container",
+                    padding: "small"
+                }}>
+                    https://&lt;your-pretix-domain&gt;/_stripe/webhook/
+                </Box>
+                <Box css={{
+                    marginBottom: "small",
+                }}>
+                    You can select "All events" when creating the webhook.
                 </Box>
             </Box>
-            <Box
-                css={{
-                    background: "container",
-                    padding: "large",
-                    borderRadius: "medium",
-                    width: "fit"
-                }}
-            >
+            <Divider/>
+            <Box css={{
+                marginTop: "large"
+            }}>
                 <Box
                     css={{
                         font: "heading",
@@ -51,14 +64,14 @@ const AppSettings = () => {
                         marginBottom: "small",
                     }}
                 >
-                    <Icon name="trash" />
+                    <Icon name="trash"/>
                     How to disconnect
                 </Box>
                 <Box>
-					You can disconnect the same way as you connect, just remove the API keys from
-					your pretix event at Settings → Payment → Stripe and pretix will make no more
-					requests to Stripe. If you want to fully remove access, use the "Uninstall"
-					button above.
+                    You can disconnect the same way as you connect, just remove the API keys from
+                    your pretix event at Settings → Payment → Stripe and pretix will make no more
+                    requests to Stripe. If you want to fully remove access, use the "Uninstall"
+                    button above.
                 </Box>
             </Box>
         </SettingsView>
